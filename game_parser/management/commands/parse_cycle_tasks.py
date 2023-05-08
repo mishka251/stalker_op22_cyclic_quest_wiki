eaef7a330173964d889ab96e1bd623606f1a8d46
mishka251 <mishkabelka251@gmail.com>
2023-05-09 00:44:08 +0500

    Улучшен парсинг рандомных наград

--- game_parser/management/commands/parse_cycle_tasks.py
+++ game_parser/management/commands/parse_cycle_tasks.py
@@ -1,9 +1,28 @@
+from pathlib import Path
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db.transaction import atomic
 
 from game_parser.logic.ltx_parser import LtxParser
-from game_parser.models import CyclicQuest, QuestRandomReward
+from game_parser.models import CyclicQuest, QuestRandomReward, Translation
+from pathlib import Path
+# from xml.etree.ElementTree import Element, parse
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.db.transaction import atomic
+
+import logging
+
+import re
+
+from lxml.etree import parse, Element, _Comment
+
+from game_parser.models import GameTask, TaskObjective, MapLocationType, Dialog, Icon
+from game_parser.models.game_story.dialog import DialogPhrase
+from PIL import Image
+from django.core.files.images import ImageFile
 
 
 class Command(BaseCommand):
@@ -50,6 +69,7 @@ class Command(BaseCommand):
                 model = self._model_from_dict(quest_name, quest_data)
             model.save()
 
+
     def _model_from_dict(self, name: str, data: dict[str, str]) -> CyclicQuest:
         game_code = name
         giver_code = name[:3]
@@ -93,4 +113,65 @@ class Command(BaseCommand):
 
     def _create_random_reward(self, name: str, data: list[str]) -> QuestRandomReward:
         possible_items_str = ';'.join(data)
-        return QuestRandomReward(name=name, possible_items_str=possible_items_str)
+        reward =  QuestRandomReward(name=name, possible_items_str=possible_items_str)
+        self._set_reward_icon(reward)
+        self._set_reward_translation(reward)
+        return reward
+
+    def _set_reward_translation(self, reward: QuestRandomReward) -> None:
+        name = reward.name
+        prefix = "random_"
+        index_str = name[len(prefix):]
+        index = int(index_str)
+        translation_prefix = "task_item_type_"
+        translation = Translation.objects.filter(code=f"{translation_prefix}{index}").first()
+        reward.name_translation = translation
+        reward.index = index
+
+    def _set_reward_icon(self, reward: QuestRandomReward) -> None:
+        name = reward.name
+        tips_icons = {
+            "random_0": [1700, 4000],
+            "random_1": [1600, 4000],
+            "random_2": [1800, 4000],
+            "random_3": [1900, 4000],
+            "random_4": [1300, 4000],
+            "random_5": [2000, 4000],
+            "random_6": [1200, 4000],
+            "random_7": [1500, 4000],
+            "random_8": [1400, 4000],
+        }
+        (icon_left, icon_top) = tips_icons[name]
+        icon_w = 100
+        icon_h = 50
+        icon_file: Path = settings.OP22_GAME_DATA_PATH/"textures"/"ui"/"ui_icon_equipment.dds"
+        image = Image.open(icon_file)
+        icon = self._get_image(image, icon_left, icon_top, icon_w, icon_h, name)
+        reward.icon = icon
+
+    def _get_image(self, image: Image, x: int, y: int, width: int, height: int, name: str) -> Icon:
+        instance: Icon = Icon(name=name)
+        box = self._get_item_image_coordinates(x, y, width, height)
+        # logger.debug(f'{box=}')
+        part = image.crop(box)
+        tmp_file_name = 'tmp.png'
+        part.save(tmp_file_name)
+        with open(tmp_file_name, 'rb') as tmp_image:
+            image_file = ImageFile(tmp_image, name=f'{name}_icon.png')
+            instance.icon = image_file
+            instance.save()
+        return instance
+
+    def _get_item_image_coordinates(self, x: int, y: int, width: int, height: int) -> tuple[int, int, int, int]:
+        inv_grid_x = x
+        inv_grid_y = y
+
+        inv_grid_width = width
+        inv_grid_height = height
+
+        left = inv_grid_x  # * self.IMAGE_PART_WIDTH
+        top = inv_grid_y  # * self.IMAGE_PART_HEIGHT
+        right = (inv_grid_x + inv_grid_width)  # * self.IMAGE_PART_WIDTH
+        bottom = (inv_grid_y + inv_grid_height)  # * self.IMAGE_PART_HEIGHT
+
+        return (left, top, right, bottom)
--- game_parser/models/quest_random_reward.py
+++ game_parser/models/quest_random_reward.py
@@ -8,12 +8,19 @@ class QuestRandomReward(models.Model):
         verbose_name = 'Случайная награда за квест'
         verbose_name_plural = 'Случайные награды за квесты'
 
+    index = models.PositiveSmallIntegerField(null=True, verbose_name="Индекс")
+
     name = models.CharField(max_length=255, verbose_name='Игровое название')
-    caption = models.CharField(max_length=255, verbose_name='Человекочитабельное название', null=True)
+    caption = models.CharField(max_length=255, verbose_name='Человекочитабельное название(из комментария)', null=True)
     possible_items_str = models.CharField(max_length=2_000, verbose_name='Названия возможных предметов')
     possible_items = models.ManyToManyField(BaseItem, verbose_name='Возможные предметы')
 
+    name_translation = models.ForeignKey("Translation", null=True, on_delete=models.SET_NULL, verbose_name="Перевод названия")
+    icon = models.ForeignKey("Icon", null=True, on_delete=models.SET_NULL, verbose_name="Иконка")
+
     def __str__(self):
+        if self.name_translation:
+            return self.name_translation.rus
         if self.caption:
             return self.caption
         return self.name
