daa0839f715de49e1ab0a4c7f377f786beae6046
mishka251 <mishkabelka251@gmail.com>
2024-02-19 22:35:41 +0500

    Переводы случайных наград + доработка админки

--- game_parser/admin/quest_random_reward.py
+++ game_parser/admin/quest_random_reward.py
@@ -1,7 +1,10 @@
-from django.contrib.admin import ModelAdmin, register
+from typing import Any, Optional
+
+from django.contrib.admin import ModelAdmin, register, display
 
 from game_parser.models import QuestRandomReward
 from game_parser.models.quest import QuestRandomRewardThrough
+from game_parser.utils.admin_utils.icon_view import icon_view
 from game_parser.utils.admin_utils.readonly_nested_table import ReadOnlyNestedTable
 
 
@@ -9,6 +12,28 @@ class CyclicQuestRandomRewardInline(ReadOnlyNestedTable):
     model = QuestRandomRewardThrough
     verbose_name = 'Выдается в ЦЗ'
     verbose_name_plural = 'Выдается в ЦЗ'
+
+
+class RandomRewardItemInline(ReadOnlyNestedTable):
+    model = QuestRandomReward.possible_items.through
+    verbose_name = 'Предметы'
+    verbose_name_plural = 'Предметы'
+    readonly_fields = [
+        "name_translation_rus",
+        "inv_icon_view",
+    ]
+
+    @display(description='Название')
+    def name_translation_rus(self, obj: Any) -> str:
+        obj = obj.baseitem
+        if obj.name_translation:
+            return obj.name_translation.rus
+        return obj.inv_name
+
+    @display(description='Иконка', )
+    def inv_icon_view(self, obj: Any) -> Optional[str]:
+        obj = obj.baseitem
+        return icon_view(obj.inv_icon)
 
 
 @register(QuestRandomReward)
@@ -25,5 +50,15 @@ class QuestRandomRewardAdmin(ModelAdmin):
     ]
 
     inlines = [
+        RandomRewardItemInline,
         CyclicQuestRandomRewardInline,
     ]
+
+    list_display = [
+        "__str__",
+        "inv_icon_view",
+    ]
+
+    @display(description='Иконка', )
+    def inv_icon_view(self, obj: QuestRandomReward) -> Optional[str]:
+        return icon_view(obj.icon.icon)
---
+++ game_parser/management/commands/fill_random_rewards_translation.py
@@ -0,0 +1,22 @@
+import logging
+from django.core.management.base import BaseCommand
+from django.db.transaction import atomic
+
+from game_parser.models import Translation, QuestRandomReward
+from game_parser.models import Treasure
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+
+    @atomic
+    def handle(self, **options):
+        count = QuestRandomReward.objects.count()
+        for index, item in enumerate(QuestRandomReward.objects.all()):
+            translation_id = f"task_item_type_{item.index}"
+            item.name = translation_id
+            item.name_translation = Translation.objects.filter(code__iexact=translation_id).first()
+            item.save()
+            print(f'{index+1}/{count}')
+
