742f27350dc0fd9f3bd0f033fc43f16f6bd37b42
mishka251 <mishkabelka251@gmail.com>
2024-03-27 02:16:18 +0500

    #20 наработки по списку квестодателей

--- game_parser/urls.py
+++ game_parser/urls.py
@@ -3,11 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from game_parser.views import TasksListView, EscapeMap
+from game_parser.views import TasksListView, EscapeMap, TaskVendorsList
 
 urlpatterns = [
     path('tasks/', TasksListView.as_view()),
     path('map/', EscapeMap.as_view()),
     path('map/<str:location>', EscapeMap.as_view()),
-    path("api/", include("game_parser.api.urls"))
+    path("api/", include("game_parser.api.urls")),
+    path("task_vendors/", TaskVendorsList.as_view())
 ]
