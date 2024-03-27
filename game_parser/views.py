020c088e505b282d8bf8f9e4730cbc29cb5867d1
mishka251 <41835667+mishka251@users.noreply.github.com>
2024-03-27 23:45:10 +0500

    #20 наработки по списку квестодателей (#24)

--- game_parser/views.py
+++ game_parser/views.py
@@ -4,7 +4,8 @@ from django.views.generic import TemplateView
 from django.db.models.functions import Lower
 
 from game_parser.logic.tasks_grouping import collect_info
-from game_parser.models import SpawnItem, Location, LocationMapInfo
+from game_parser.models import SpawnItem, Location, LocationMapInfo, CycleTaskVendor
+from game_parser.models.quest import QuestKinds
 
 
 class TasksListView(TemplateView):
@@ -58,3 +59,26 @@ class EscapeMap(TemplateView):
             "name": name,
             "section_name": section_name,
         }
+
+
+class TaskVendorsList(TemplateView):
+    template_name = "task_vendors_list/task_vendor_list.html"
+
+    def get_context_data(self, **kwargs):
+        vendors = CycleTaskVendor.objects.all()
+        return {
+            "vendors": [self._vendor_to_dict(vendor) for vendor in vendors]
+        }
+
+    def _vendor_to_dict(self, vendor: CycleTaskVendor) -> dict:
+        npc_profile = vendor.get_npc_profile()
+        return {
+            "image_path": self._get_npc_profile_icon(npc_profile) if npc_profile else None,
+            "name": npc_profile.name_translation.rus if npc_profile and npc_profile.name_translation else None,
+            "tasks_count": vendor.cyclicquest_set.count(),
+            "has_chain": vendor.cyclicquest_set.filter(type=QuestKinds.chain).exists(),
+            "quests_link": "",
+        }
+
+    def _get_npc_profile_icon(self, npc_profile):
+        return npc_profile.icon and npc_profile.icon.icon and npc_profile.icon.icon.url
