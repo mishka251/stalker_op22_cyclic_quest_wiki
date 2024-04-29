f376f20c1a3dcc4b100e8720a811310d7bfa5a7d
mishka251 <mishkabelka251@gmail.com>
2024-04-30 00:33:06 +0500

    38 правка переноса данных

--- stalker_op22_cyclic_quest_wiki/management/commands/import_data.py
+++ stalker_op22_cyclic_quest_wiki/management/commands/import_data.py
@@ -1,5 +1,3 @@
-import dataclasses
-import os
 import shutil
 import zipfile
 from pathlib import Path
@@ -7,22 +5,32 @@ from pathlib import Path
 import tablib
 from django.conf import settings
 from django.core.management import BaseCommand, CommandError
-from django.db.models import Model
 from import_export.results import Error
 
 from stalker_op22_cyclic_quest_wiki.management.commands.resources import to_export
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        # parser.add_argument('--file_path', type=str, help='Path to the file', default='data.zip')
+        parser.add_argument(
+            '--imported_archive',
+            type=Path,
+            help='Path to imported file',
+            default=Path('data.zip'),
+        )
+
     def handle(self, *args, **options):
+        imported_archive = options['imported_archive']
         tmp_dir = Path("import_tmp")
         tmp_dir.mkdir(exist_ok=True)
 
-        with zipfile.ZipFile('data.zip', 'r') as myzip:
+        with zipfile.ZipFile(imported_archive, 'r') as myzip:
             myzip.extractall(tmp_dir)
         for model_info in to_export:
             print(f"start {model_info.model_cls.__name__}")
-            file_path = tmp_dir/model_info.file_name
+            file_path = tmp_dir / model_info.file_name
             resource = model_info.resource_cls()
             with open(file_path, "r", encoding="utf-8") as file:
                 dataset = tablib.Dataset().load(file.read(), format="csv")
@@ -44,23 +52,23 @@ class Command(BaseCommand):
                 print(f"end {model_info.model_cls.__name__} OK")
 
         print("Start import icons")
-        icons_dir = tmp_dir/"icons"
+        icons_dir = tmp_dir / "icons"
         media_dir = Path(settings.MEDIA_ROOT)
         media_dir.mkdir(exist_ok=True)
         for icon_path in icons_dir.iterdir():
             if icon_path.is_dir():
-                shutil.copytree(icon_path, media_dir/icon_path.name, dirs_exist_ok=True)
+                shutil.copytree(icon_path, media_dir / icon_path.name, dirs_exist_ok=True)
             else:
-                shutil.copyfile(icon_path, media_dir/icon_path.name)
+                shutil.copyfile(icon_path, media_dir / icon_path.name)
         print("End import icons")
 
         print("Start import maps")
-        maps_dir = tmp_dir/"maps"
+        maps_dir = tmp_dir / "maps"
         for map_path in maps_dir.iterdir():
             if map_path.is_dir():
-                shutil.copytree(map_path, media_dir/map_path.name, dirs_exist_ok=True)
+                shutil.copytree(map_path, media_dir / map_path.name, dirs_exist_ok=True)
             else:
-                shutil.copyfile(map_path, media_dir/map_path.name)
+                shutil.copyfile(map_path, media_dir / map_path.name)
         print("End import maps")
         shutil.rmtree(tmp_dir)
 
