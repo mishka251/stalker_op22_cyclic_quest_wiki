000e4c26e2fdae8e4731debaeda65f945c810b9a
mishka251 <mishkabelka251@gmail.com>
2024-04-30 13:29:53 +0500

    39 добавил sentry

--- deploy/settings_template.py
+++ deploy/settings_template.py
@@ -16,3 +16,16 @@ DATABASES = {
 }
 
 STATIC_ROOT=BASE_DIR/"staticfiles"
+
+sentry_sdk.init(
+    dsn=...,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=0.5,
+    enable_tracing=True,
+    environment="production",
+)
