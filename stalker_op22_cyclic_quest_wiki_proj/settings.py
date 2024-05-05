434e3cd81e09d41120227670dcdbc4c9155be5f3
mishka251 <mishkabelka251@gmail.com>
2024-05-05 14:19:41 +0500

    14 Правка настроек

--- stalker_op22_cyclic_quest_wiki_proj/settings.py
+++ stalker_op22_cyclic_quest_wiki_proj/settings.py
@@ -26,10 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-
 DEBUG = os.getenv("DEBUG", "False") == "True"
-print(os.getenv("DEBUG", "False"), DEBUG)
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
