4e971c37afdd2b743d021f2c7e4a3b0c2839d9ff
mishka251 <41835667+mishka251@users.noreply.github.com>
2024-05-10 11:05:49 +0500

    Правки для версии 0.3.0 (#51)

--- deploy/build.ps1
+++ deploy/build.ps1
@@ -1,5 +1,5 @@
-docker image rm mishka251/stalker_op22_cyclic_quest_wiki-django:v0.2.1
-docker image rm mishka251/stalker_op22_cyclic_quest_wiki-nginx:v0.2.1
+docker image rm mishka251/stalker_op22_cyclic_quest_wiki-django:v0.3.0
+docker image rm mishka251/stalker_op22_cyclic_quest_wiki-nginx:v0.3.0
 
-docker image build -f ./deploy/django/Dockerfile -t mishka251/stalker_op22_cyclic_quest_wiki-django:v0.2.1 .
-docker image build -f ./deploy/nginx/Dockerfile -t mishka251/stalker_op22_cyclic_quest_wiki-nginx:v0.2.1 .
+docker image build -f ./deploy/django/Dockerfile -t mishka251/stalker_op22_cyclic_quest_wiki-django:v0.3.0 .
+docker image build -f ./deploy/nginx/Dockerfile -t mishka251/stalker_op22_cyclic_quest_wiki-nginx:v0.3.0 .
