e8028060695d5e4fbfebc7de60bd313d33213574
mishka251 <41835667+mishka251@users.noreply.github.com>
2024-05-13 19:59:04 +0500

    52 items list (#54)
    * #52 список предметов - первая версия
    
    * #52 поиск использования/получения предметов - первая версия
    
    * #52 мелкие правки

--- stalker_op22_cyclic_quest_wiki/urls.py
+++ stalker_op22_cyclic_quest_wiki/urls.py
@@ -7,6 +7,11 @@ from stalker_op22_cyclic_quest_wiki.views.cyclic_quests.vendors_list import (
     TaskVendorsList,
 )
 from stalker_op22_cyclic_quest_wiki.views.index import IndexView
+from stalker_op22_cyclic_quest_wiki.views.items.list import ItemsListView
+from stalker_op22_cyclic_quest_wiki.views.items.where_find_item import WhereFindItemView
+from stalker_op22_cyclic_quest_wiki.views.items.where_item_needed import (
+    WhereItemNeededView,
+)
 
 urlpatterns = [
     path("task_vendors/", TaskVendorsList.as_view(), name="task_vendors"),
@@ -14,6 +19,17 @@ urlpatterns = [
         "task_vendors/<int:vendor_id>/quests/",
         VendorQuestsList.as_view(),
         name="vendor_tasks",
+    ),
+    path("items/", ItemsListView.as_view(), name="items"),
+    path(
+        "items/<int:item_id>/where_needed",
+        WhereItemNeededView.as_view(),
+        name="where_item_needed",
+    ),
+    path(
+        "items/<int:item_id>/where_find",
+        WhereFindItemView.as_view(),
+        name="where_find_item",
     ),
     path("", IndexView.as_view(), name="index"),
 ]
